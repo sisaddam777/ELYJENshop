09ed278d43a1e1478ad467ac161325e258c60188
Imran <imranshuvo101@gmail.com>
2026-07-05 14:38:45 +0600

    deduplication issue

--- src/app/components/FacebookPixel.tsx
+++ src/app/components/FacebookPixel.tsx
@@ -65,6 +65,7 @@ export default function FacebookPixel({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('set', 'autoConfig', false, '${sanitizedPixelId}');
             fbq('init', '${sanitizedPixelId}');
           `,
         }}
